e74e2380f1a7e81a1720a44d9ad4ce2204ef13da
NashSkinner <nash@Gray.local>
2020-08-14 17:44:53 -0500

    Adding all of the project files to build new branches for

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import NashSkinner from './NashSkinner.png';
+import React, { Component } from 'react';
 import './App.css';
+import About from './components/about'
+import Introduction from './components/introduction'
+import Projects from './components/projects'
+import Sidebar from './components/sidebar'
+import Timeline from './components/timeline'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={NashSkinner} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="colorlib-page">
+        <div id="container-wrap">
+		<Sidebar></Sidebar>
+		<div id="colorlib-main">
+			<Introduction></Introduction>
+			<About></About>
+			<Projects></Projects>
+			<Timeline></Timeline>
+          	</div>
+      	</div>
+      </div>
+    );
+  }
 }
 
 export default App;
