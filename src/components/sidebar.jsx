4177b7151709586758daf652d3799e38841975d1
Nash Skinner <nash.skinner@yahoo.com>
2020-08-27 18:14:18 -0500

    Different base for side bar. Png now transparent.

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,47 +1,46 @@
 import React, { Component } from 'react'
-import Sidebar from 'react-sidebar'
-
-const mql = window.matchMedia(`(min-width: 800px)`);
+import {Sidebar, InputItem, DropdownItem, Icon, Item, Logo, LogoText} from 'react-sidebar-ui'
+import 'react-sidebar-ui/dist/index.css';
+import image from './../NashSkinner.png'
 
 export default class sidebar extends Component {
+  render() {
+    return (
+    <div>
+      <Sidebar bgColor='black' isCollapsed={false}>
+        <Logo
+          image={image}
+          imageName='react logo'/>
+        <LogoText>Nash Skinner</LogoText>
+        <DropdownItem
+          values={['First', 'Second', 'Third']}
+          bgColor={'black'}>
+          Menu
+        </DropdownItem>
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      sidebarDocked: mql.matches,
-      sidebarOpen: false
-    };
- 
-    this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
-    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+        <Item bgColor='black'>
+          <Icon><i className="fas fa-home"/></Icon>
+          Home
+        </Item>
+        <Item bgColor='black'>
+          <Icon><i className="fas fa-info"/></Icon>
+          About
+        </Item>
+        <Item bgColor='black'>
+          <Icon><i className="fas fa-sitemap"/></Icon>
+          My Website
+        </Item>
+        <Item bgColor='black'>
+          <Icon><i className="far fa-address-book"/></Icon>
+          Contacts
+        </Item>
+        <Item bgColor='black'>
+          <Icon><i className="fas fa-rss-square"/></Icon>
+          Blog
+        </Item>
+        <InputItem type='text' placeholder={'Search...'}/>
+      </Sidebar>
+    </div>
+    )
   }
- 
-  componentWillMount() {
-    mql.addListener(this.mediaQueryChanged);
-  }
- 
-  componentWillUnmount() {
-    this.state.mql.removeListener(this.mediaQueryChanged);
-  }
- 
-  onSetSidebarOpen(open) {
-    this.setState({ sidebarOpen: open });
-  }
- 
-  mediaQueryChanged() {
-    this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
-  }
-
-    render() {
-      return (
-        <Sidebar
-          sidebar={<b>Sidebar content</b>}
-          open={this.state.sidebarOpen}
-          docked={this.state.sidebarDocked}
-          onSetOpen={this.onSetSidebarOpen}
-        >
-          <b>Main content</b>
-        </Sidebar>
-      );
-    }
-}
+};
